f49d8de37fd3d001d755f2cc4c22f3af8999064c
wandl-6A72h <hiwepy@gmail.com>
2026-08-08 23:58:29 +0800

    chore: propagate pom standardization, tests, javadoc and @author fix from 4.1.x
    - Add jacoco-maven-plugin 0.8.15 with 90% coverage gate
    - Add central-publishing-maven-plugin 0.11.0
    - Update surefire (enable tests, argLine for JaCoCo, *_Test.java pattern)
    - Fix SCM URLs
    - Copy test files from 4.1.x branch
    - Copy Javadoc improvements from 4.1.x branch
    - Standardize @author to [@Loong Wan](https://github.com/loong10k)
    - Propagated to 4.0.x branch

--- src/main/java/org/springframework/security/boot/dingtalk/authentication/DingTalkTmpCodeAuthenticationProcessingFilter.java
+++ src/main/java/org/springframework/security/boot/dingtalk/authentication/DingTalkTmpCodeAuthenticationProcessingFilter.java
@@ -38,7 +38,7 @@ import java.io.IOException;
  *  企业内部应用免登：https://open.dingtalk.com/document/orgapp-server/enterprise-internal-application-logon-free
  *  第三方企业应用免登： https://open.dingtalk.com/document/orgapp-server/third-party-enterprise-application-logon-free
  *  应用管理后台免登: https://open.dingtalk.com/document/orgapp-server/log-on-site-application-management-backend
- * @author 		： <a href="https://github.com/hiwepy">wandl</a>
+ * @author [@Loong Wan](https://github.com/loong10k)
  */
 public class DingTalkTmpCodeAuthenticationProcessingFilter extends AbstractAuthenticationProcessingFilter {
 
