2e6681b12a426bc0f509050ef0ada9cd0c5bcc68
loong10k <20489781+loong10k@users.noreply.github.com>
2026-08-10 16:55:17 +0800

    docs: unify @author tag to @author <a href="https://github.com/loong10k">Loong Wan</a>

--- src/main/java/org/springframework/security/boot/dingtalk/authentication/DingTalkTmpCodeLoginRequest.java
+++ src/main/java/org/springframework/security/boot/dingtalk/authentication/DingTalkTmpCodeLoginRequest.java
@@ -7,7 +7,7 @@ import lombok.Data;
 
 /**
  * 企业内部应用免登、第三方企业应用免登、应用管理后台免登
- * @author [@Loong Wan](https://github.com/loong10k)
+ * @author <a href="https://github.com/loong10k">Loong Wan</a>
  */
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
